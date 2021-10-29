0dbd2894217399642d797484628469dd5921123a
Kelvin Isievwore <kelvinator4leo@gmail.com>
2021-10-29 19:58:46 +0100

    Implemented All

--- server.js
+++ server.js
@@ -4,8 +4,15 @@ const cors = require("cors");
 
 const app = express();
 
+const db = require("./app/models");
+db.sequelize.sync();
+
+db.sequelize.sync({ force: true }).then(() => {
+    console.log("Drop and re-sync db.");
+});
+
 var corsOptions = {
-	origin: "http://localhost:8081",
+    origin: "http://localhost:8081",
 };
 
 app.use(cors(corsOptions));
@@ -18,11 +25,13 @@ app.use(express.urlencoded({ extended: true }));
 
 // simple route
 app.get("/", (req, res) => {
-	res.json({ message: "Welcome to geeky application." });
+    res.json({ message: "Welcome to geeky application." });
 });
+
+require("./app/routes/tutorial.routes")(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-	console.log(`Server is running on port ${PORT}.`);
+    console.log(`Server is running on port ${PORT}.`);
 });
